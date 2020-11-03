80a3de6048a16ee0bc93cf3979c11b99ef66b07c
Gamapro <0223826@up.edu.mx>
2020-11-03 14:48:09 -0600

    AlmostPython

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import sys
 
 vars = {} # diccionario de variables {"nombre de variable": "valor de varialbe"}
 
-def declare(line):
+def Declare(line):
     pass
 
-def assign(line):
+def Assign(line):
     pass
 
 def Print(line):
     pts = line.split(" ")
     print(pts[1])
 
-def execute_line(line):
+def Execute_line(line):
     # funcion para ejecutar una line
     # esta funcion llama a las funcinoes de palabras reservadas
     pass
