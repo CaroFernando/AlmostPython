b654ef2b35fc0edbcfbd2ad1e2d7c1078cc843aa
Gamapro <0223826@up.edu.mx>
2020-11-03 16:53:03 -0600

    AlmostPython

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+# LIBRARIES
+
 import sys
 
-vars = {} # diccionario de variables {"nombre de variable": "valor de varialbe"}
+
+# VARIABLES
+
+vars = dict() # diccionario de variables {"nombre de variable": "valor de varialbe"}
+i = 0
+accion = ''
+
+# FUNCTIONS
+
+# Declaracion y Asignacion
 
 def Declare(line):
     pass
@@ -8,9 +19,20 @@ def Declare(line):
 def Assign(line):
     pass
 
+def Delete():
+    pass
+
+def Read():
+    pass
+
 def Print(line):
     pts = line.split(" ")
     print(pts[1])
+
+def Return():
+    
+
+# Estructuras de Control
 
 def Execute_line(line):
     # funcion para ejecutar una line
@@ -22,10 +44,28 @@ def For():
     # estas funciones llaman a execute_line
     pass
 
-file_name = sys.argv[1] # nombre del archivo que se da desde terminal "python3 main.py nombre.txt#
 
-file = open(file_name, "r")
-print(file.read())
 
-# wea para leer los 
+# MAIN
+
+if __name__ == "__main__":
+
+    file_name = sys.argv[1] # nombre del archivo que se da desde terminal "python3 main.py nombre.txt#
+
+    file = open(file_name, "r")
+    for x in file:
+        # Quitar el salto de linea al final
+        x = x[:len(x)-1]
+        # Separar los comando por espacios 
+        comandos = x.split(" ");
+        if comandos == [''] or len(comandos) == 0:
+            continue
+        # Leo la accion a ajecutar, y la quito de mis comandos
+        accion = comandos[0]
+        comandos = comandos[1:]
+        
+        for comando in comandos:
+            print(comando, end = ' ')
+        print()
+
 
