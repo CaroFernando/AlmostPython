c0e8ea7bee8f4089b6ea1daca31a8456db28366b
CaroFernando <luisfer.caro@hotmail.com>
2020-11-04 12:05:21 -0600

    Control structures

--- AlmostPython.py
+++ AlmostPython.py
@@ -1,6 +1,7 @@
 # LIBRARIES
 
 import sys
+from ControlStructures import Execute_block
 
 
 # VARIABLES
@@ -24,7 +25,6 @@ def Execute_line(line):
 def For():
 """
 
-
 # MAIN
 
 if __name__ == "__main__":
@@ -32,25 +32,7 @@ if __name__ == "__main__":
     file_name = sys.argv[1] # nombre del archivo que se da desde terminal "python3 main.py nombre.txt#
 
     file = open(file_name, "r")
-    for x in file:
-        # Quitar el salto de linea al final
-        x = x[:len(x)-1]
-        # Separar los comando por espacios 
-        comandos = x.split(" ");
-        if comandos == [''] or len(comandos) == 0:
-            continue
-        # Leo la accion a ajecutar, y la quito de mis comandos
-        accion = comandos[0]
-        comandos = comandos[1:]
-        
-        print(comandos)
-        """for comando in comandos:
-            print(comando, end = ' ')
-        print()"""
-
-        if accion == "Declare":
-            Declare(comandos);
-
-
+    lines = [i for i in file]
+    Execute_block(lines)
     file.close()
         
